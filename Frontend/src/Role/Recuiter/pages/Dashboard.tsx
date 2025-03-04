3821b8173b3f92ca0f2442dfa6a079af984c430a
Harankaha Arachchige Ihansa Sansiluni Leelarathna <haisleelarathna@gmail.com>
2025-03-04 20:30:33 +0530

    commit

--- Frontend/src/Role/Recuiter/pages/Dashboard.tsx
+++ Frontend/src/Role/Recuiter/pages/Dashboard.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
 
 const dashboard = () => {
   return (
+    <div>
+      <NavBar />
     <div>dashboard</div>
+    </div>
   )
 }
 
