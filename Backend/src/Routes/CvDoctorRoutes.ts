9939495a7585a50e061c09d160ffb2f86be19f9e
mokke2002 <89510318+mokke2002@users.noreply.github.com>
2025-03-16 13:19:08 +0530

    feat: refactor Doctor CV routes to use controller methods for improved organization and maintainability

--- Backend/src/Routes/CvDoctorRoutes.ts
+++ Backend/src/Routes/CvDoctorRoutes.ts
@@ -1,53 +1,12 @@
 import express from 'express';
 import CvDoctorUpdate from '../models/CvUpdate';
 const router = express.Router();
+const docCvController=require('../controllers/cvController');
 
-router.get("/viewDoctorsCv",async(req,res)=>{
-    const result=await CvDoctorUpdate.find();
-    if(result){
-        res.send({
-            
-        "Message":`${result.length} Doctors found`,
-        "CvDoctorUpdate":result
-    }).status(200);
-    }else{
-        res.send({
-            "Message":"No Doctors found"
-        }).status(404);
-    }
-});
-
-router.post("/addDoctorCv",async(req,res)=>{
-    const data=req.body;
-    const doctor=new CvDoctorUpdate(data);
-    const result=await doctor.save();
-    if(result){
-        res.send({
-            "Message":"Doctor Cv added successfully",
-            "Doctor ID":result._id
-        }).status(201);
-    }else{
-        res.send({
-            "Message":"Doctor Cv not added"
-        }).status(500);
-    }
-});
-
-router.put("/ReplaceCv/:id",async(req,res)=>{
-    const id=req.params.id;
-    const deta=req.body;
-
-    const result=await CvDoctorUpdate.findOneAndReplace({_id:id},deta,{new:true});
-    if(result){
-        res.send({
-            "Message":"Doctor Cv updated successfully",
-            "Doctor ID":result
-        }).status(200);
-    }else{
-        res.send({
-            "Message":"Doctor Cv not updated"
-        }).status(500);
-    }
-});
+router.get("/viewDoctorsCv",docCvController.viewDoctorsCv);
+router.post("/addDoctorCv",docCvController.addDoctorCv);
+router.put("/ReplaceCv/:id",docCvController.ReplaceCv);
+router.patch("/updateCv/:id",docCvController.updateCv);
+router.delete("/deleteCv/:id",docCvController.deleteCv);
 
 export default router;
