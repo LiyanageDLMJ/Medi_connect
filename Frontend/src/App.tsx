d2a99a2e5efae577fec5b56778945c0d25ccd798
nismygaffoor <nismygaffoor5@gmail.com>
2025-04-07 16:57:52 +0530

    update
    app update

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,34 +1,56 @@
-import { Routes, Route } from "react-router-dom";
-import JobInternshipSearch from "./Role/Physician/pages/JobInternshipSearch";
 
+
+
+
+import { Routes, Route, Outlet  } from "react-router-dom";
+import JobInternshipSearch from "./Role/Physician/pages/JobInternshipSearch";
+import DegreeList from "./Role/higherEducation/pages/example";
 import HigherEducationSearch from "./Role/Physician/pages/HigherEducationSearch";
 import JobApplicationForm from "./Role/Physician/pages/JobApplicationForm";
 import DegreeListing from "./Role/higherEducation/pages/DegreeListing";
-import Login from"./LoginRegister/login/Login"
+import ViewApplications from "./Role/higherEducation/pages/ViewApplications";
+import Messages from "./Role/higherEducation/pages/Messages";
+import Login from "./LoginRegister/login/Login";
 import Register from "./LoginRegister/register/Register";
-import Footer from "./Components/FooterDiv/Footer"
 import JobPost from "./Role/Recuiter/pages/JobPost";
+import "react-datepicker/dist/react-datepicker.css";
 import Home from "./LoginRegister/Home/Home";
 import ForgotPassword from "./LoginRegister/login/ForgetPassword";
+import WithFooter from "./Layout/WithFooter";
+import WithoutFooter from "./Layout/WithoutFooter";
 
 const App = () => {
   return (
-    <div>
-      
-      <Routes>
-     
-        <Route path="/higher-education" element={<HigherEducationSearch />} />
-        <Route path="/physician/job-internship" element={<JobInternshipSearch />} />
-        <Route path="/degree-listing" element={<DegreeListing />} />
-        <Route path="/physician/job-application" element={<JobApplicationForm />} />
-        <Route path="login" element={<Login />} />
+    <Routes>
+      <Route element={<WithFooter><Outlet /></WithFooter>}>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/recuiter/jobPost" element={<JobPost />} />
-      </Routes>
-      <Footer />
-    </div>
+      </Route>
+
+      <Route element={<WithoutFooter><Outlet /></WithoutFooter>}>
+
+        <Route path="/physician">
+          <Route path="higher-education" element={<HigherEducationSearch />} />
+          <Route path="job-internship" element={<JobInternshipSearch />} />
+          <Route path="job-application" element={<JobApplicationForm />} />
+        </Route>
+
+        <Route path="/higher-education">
+          <Route path="messages" element={<Messages />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+          <Route path="degree-listing" element={<DegreeListing />} />
+        </Route>
+
+        <Route path="/recuiter">
+          <Route path="jobPost" element={<JobPost />} />
+        </Route>
+
+        <Route path="/degreelist" element={<DegreeList />} />
+      </Route>
+    </Routes>
+
   );
 };
 
