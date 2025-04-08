5651a11961c3ed5815b4cd456227a4862d030371
nismygaffoor <nismygaffoor5@gmail.com>
2025-04-08 19:52:36 +0530

    Update PerformanceInsights.tsx

--- Frontend/src/Role/higherEducation/pages/PerformanceInsights.tsx
+++ Frontend/src/Role/higherEducation/pages/PerformanceInsights.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import Sidebar from "../components/Sidebar";
+import TopBar from "../components/TopBar";
+
 
 const PerformanceInsights = () => {
   return (
@@ -9,9 +11,14 @@ const PerformanceInsights = () => {
     {/* Sidebar */}
     <Sidebar />
 
-    {/* Main Content */}
-    <div className="flex-1 overflow-auto md:pl-64"> {/* Add padding on larger screens to account for sidebar */}
-</div>
+   {/* Main Content */}
+   <div className="flex-1 overflow-auto md:pl-64"> {/* Add padding on larger screens to account for sidebar */}
+    <TopBar />
+    <div className="flex flex-col min-h-[calc(100vh-80px)] p-4 ">
+     
+     
+     
+      </div></div>
 </div>
   )
 }
