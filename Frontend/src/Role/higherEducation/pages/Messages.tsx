d1b919d47b06d37a3fa79395040b58399733c982
nismygaffoor <nismygaffoor5@gmail.com>
2025-04-08 19:52:29 +0530

    Update Messages.tsx

--- Frontend/src/Role/higherEducation/pages/Messages.tsx
+++ Frontend/src/Role/higherEducation/pages/Messages.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebar from "../components/Sidebar";
+import TopBar from "../components/TopBar";
+
 
 const Messages = () => {
   return (
@@ -7,9 +9,16 @@ const Messages = () => {
     {/* Sidebar */}
     <Sidebar />
 
+    
     {/* Main Content */}
     <div className="flex-1 overflow-auto md:pl-64"> {/* Add padding on larger screens to account for sidebar */}
-</div>
+    <TopBar />
+    <div className="flex flex-col min-h-[calc(100vh-80px)] p-4 ">
+     
+     
+     
+      </div>
+      </div>
 </div>
   )
 }
