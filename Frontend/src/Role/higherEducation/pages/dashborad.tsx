8e61f120de01ef0f3d983499f07c4e52b2164eb3
nismygaffoor <nismygaffoor5@gmail.com>
2025-04-08 19:41:04 +0530

    Update dashborad.tsx

--- Frontend/src/Role/higherEducation/pages/dashborad.tsx
+++ Frontend/src/Role/higherEducation/pages/dashborad.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebar from "../components/Sidebar";
+import TopBar from "../components/TopBar";
+
 
 const Dashborad = () => {
   return (
@@ -9,6 +11,12 @@ const Dashborad = () => {
 
     {/* Main Content */}
     <div className="flex-1 overflow-auto md:pl-64"> {/* Add padding on larger screens to account for sidebar */}
+    <TopBar />
+    <div className="flex flex-col min-h-[calc(100vh-80px)] p-4 ">
+     
+     
+     
+      </div>
 </div>
 </div>
   )
