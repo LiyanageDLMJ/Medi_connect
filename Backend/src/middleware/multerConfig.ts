e795f7d47e3b964ff7fdb5c7cb7664d3b2126167
nismygaffoor <nismygaffoor5@gmail.com>
2025-04-30 21:47:33 +0530

    Update multerConfig.ts

--- Backend/src/middleware/multerConfig.ts
+++ Backend/src/middleware/multerConfig.ts
@@ -25,4 +25,21 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
 });
 
+const imageStorage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "image"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+}); 
+
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter: (req, file, cb) => {
+    const types = ["image/jpeg", "image/png", "image/jpg"];
+    types.includes(file.mimetype)
+      ? cb(null, true)
+      : cb(new Error("Only image files are allowed"));
+  },
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+});
+
 export default upload;
