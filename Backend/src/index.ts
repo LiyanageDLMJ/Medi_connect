67897e1f0ed3afbe474467ca9812dd6c3f337445
nismygaffoor <nismygaffoor5@gmail.com>
2025-05-01 18:15:31 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -8,7 +8,9 @@ import connectDB from "./Config/db";
 import JobSearch from "./Routes/PhysicianRoutes/JobSearchRoutes";
 import jobApplicationRoutes from "./Routes/PhysicianRoutes/jobApplicationRoutes";
 
-import DegreeApplicationRoutes from "./Routes/PhysicianRoutes/DegreeApplicationRoutes";
+import DegreeApplicationRoutes from "./Routes/PhysicianRoutes/degreeApplicationRoutes";
+
+// import ViewDegreeApplicationRoutes from "./Routes/EducationRoutes/ViewDegreeApplicationRoutes";
 import degreeListingRoutes from './Routes/EducationRoutes/DegreeListingRoutes';
 import higherEducationRoutes from './Routes/EducationRoutes/higherEducationRoutes';
 import fs from "fs";
@@ -47,6 +49,7 @@ app.use("/JobApplication", jobApplicationRoutes);
 app.use("/degrees", degreeListingRoutes);
 app.use("/higherDegrees", higherEducationRoutes);
 app.use('/degreeApplications', DegreeApplicationRoutes);
+// app.use('/viewDegreeApplications', ViewDegreeApplicationRoutes);
 // app.use('/images', express.static('src/image'));
 app.use('/image', express.static(path.join(__dirname, "../image")));
 
