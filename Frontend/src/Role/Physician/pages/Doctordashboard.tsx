185f4a9e12b8d49e7389617b3eae2df96ac355a9
nismygaffoor <nismygaffoor5@gmail.com>
2025-07-27 23:57:08 +0530

    Update Doctordashboard.tsx

--- Frontend/src/Role/Physician/pages/Doctordashboard.tsx
+++ Frontend/src/Role/Physician/pages/Doctordashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { Bell, Search } from "lucide-react";
 import Sidebar from "../components/NavBar/Sidebar";
 import doctorImage from "../../../Doctor.png";
